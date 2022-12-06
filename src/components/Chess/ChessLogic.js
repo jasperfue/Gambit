5256bf59ac77f43769507830d4e993b98237580f
jasperfue <jasperfue@live.de>
2022-12-06 15:55:38 +0100

    Promotion

--- src/components/Chess/ChessLogic.js
+++ src/components/Chess/ChessLogic.js
@@ -2,7 +2,7 @@ import { Chessground } from 'chessground'
 import { Chess, SQUARES } from 'chess.js';
 
 
-export function validMoves(chess) {
+export function getValidMoves(chess) {
     const moves = new Map;
     for (var square of SQUARES) {
         const ms = chess.moves({square: square, verbose: true});
@@ -28,7 +28,7 @@ export function refreshBoard(ground, chess) {
     ground.set({
         turnColor: toColor(chess),
         movable: {
-            dests: validMoves(chess)
+            dests: getValidMoves(chess)
         }
     });
 }
@@ -36,5 +36,30 @@ export function refreshBoard(ground, chess) {
 
 function toColor(chess) {
     return (chess.turn() === 'w') ? 'white' : 'black';
+}
+
+export function charPieceToString(char) {
+    var piece;
+    switch(char) {
+        case 'q':
+            piece = 'queen';
+            break;
+        case 'r':
+            piece = 'rook';
+            break;
+        case 'b':
+            piece = 'bishop';
+            break;
+        case 'n':
+            piece = 'knight';
+            break;
+        case 'p':
+            piece = 'prawn';
+            break;
+        case 'k':
+            piece = 'king';
+            break;
+    }
+    return piece;
 }
 
