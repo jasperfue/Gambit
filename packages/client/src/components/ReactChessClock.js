938d58bfb03443b7161a011924f90d2c5806183c
jaspe <jasperfue@live.de>
2023-03-01 16:47:47 +0100

    Gastspiele wieder möglich

--- packages/client/src/components/ReactChessClock.js
+++ packages/client/src/components/ReactChessClock.js
@@ -1,22 +1,23 @@
 import React, {useEffect, useRef, useState} from "react";
+
 const ReactChessClock = (props) => {
     const [time, setTime] = useState(props.time);
     const [timeWhite, setTimeWhite] = useState(props.remainingTimeWhite
         ? props.remainingTimeWhite.minutes * 60 + props.remainingTimeWhite.seconds
-        : time.minutes * 60);
+        : props.time.minutes * 60);
     const [timeBlack, setTimeBlack] = useState(props.remainingTimeBlack
-    ? props.remainingTimeBlack.minutes * 60 + props.remainingTimeBlack.seconds
-: time.minutes * 60);
+        ? props.remainingTimeBlack.minutes * 60 + props.remainingTimeBlack.seconds
+        : props.time.minutes * 60);
     const [orientation, setOrientation] = useState(props.orientation);
     const [socket, setSocket] = useState(props.socket);
     const currentTimer = useRef(0);
-    const [currentTurn,setCurrentTurn] = useState(props.currentState);
+    const [currentTurn, setCurrentTurn] = useState(props.currentState);
     const [startingTimeWhite, setStartingTimeWhite] = useState(props.startingTimeWhite);
     const [startingTimeBlack, setStartingTimeBlack] = useState(props.startingTimeBlack);
 
     const decrease = (setFunction) => {
         setFunction(seconds => {
-            if(seconds === 0) {
+            if (seconds === 0) {
                 setCurrentTurn('off');
             } else {
                 return seconds - 1;
@@ -26,7 +27,8 @@ const ReactChessClock = (props) => {
 
     useEffect(() => {
         console.log('toggle');
-        let setFunction = () => {};
+        let setFunction = () => {
+        };
         switch (currentTurn) {
             case 'tw':
                 setFunction = setTimeWhite;
@@ -46,7 +48,7 @@ const ReactChessClock = (props) => {
                 return;
         }
         const id = setInterval(() => {
-            if(currentTurn === 'off') {
+            if (currentTurn === 'off') {
                 clearInterval(id);
                 return;
             }
@@ -69,7 +71,7 @@ const ReactChessClock = (props) => {
             stopClocks();
             setCurrentTurn('sb');
         });
-        socket.on('stop_starting_time_black', ()  => {
+        socket.on('stop_starting_time_black', () => {
             stopClocks();
             setCurrentTurn('tw');
         })
@@ -84,7 +86,7 @@ const ReactChessClock = (props) => {
 
 
     function stopClocks() {
-        if(currentTimer.current !== 0) {
+        if (currentTimer.current !== 0) {
             clearInterval(currentTimer.current);
             currentTimer.current = 0;
         }
@@ -98,39 +100,63 @@ const ReactChessClock = (props) => {
 
     return (
         <>
-        {orientation === 'white' ?
-            <>
-                {currentTurn.includes('s')
-                ?
-                    <p>{startingTimeBlack}</p>
-                :
-                <> </>}
-                <div style={{display: "flex", flexDirection: "row", backgroundColor: 'whitesmoke', marginTop:'1VH', padding:25}}>
-                    <p id={'Black'}>{(timeBlack / 60 > 9 ? Math.trunc(timeBlack / 60) : '0' + Math.trunc(timeBlack / 60)) + ":" +
-                    (timeBlack % 60 > 9 ? timeBlack % 60 : '0' + timeBlack % 60)}</p>
-                </div>
-                <div style={{display: "flex", flexDirection: "row", backgroundColor: 'whitesmoke', marginTop:'1VH', padding:25}}>
-                <p id={'White'}>{(timeWhite / 60 > 9 ? Math.trunc(timeWhite / 60) : '0' + Math.trunc(timeWhite / 60)) + ":" +
-                (timeWhite % 60 > 9 ? timeWhite % 60 : '0' + timeWhite % 60)}</p>
-            </div>
-                {currentTurn === "sw"
-                ?
-                <p>{startingTimeWhite}</p>
-                :
-                <> </>}
-            </>
-            :
+            {orientation === 'white' ?
                 <>
-                    {currentTurn === "sw"
-                    ?
-                    <p>{startingTimeWhite}</p>
-                    :
-                    <> </>}
-                    <div style={{display: "flex", flexDirection: "row", backgroundColor: 'whitesmoke', marginTop:'1VH', padding:25}}>
+                    {currentTurn.includes('s')
+                        ?
+                        <p>{startingTimeBlack}</p>
+                        :
+                        <> </>}
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        backgroundColor: 'whitesmoke',
+                        marginTop: '1VH',
+                        padding: 25
+                    }}>
+                        <p id={'Black'}>{(timeBlack / 60 > 9 ? Math.trunc(timeBlack / 60) : '0' + Math.trunc(timeBlack / 60)) + ":" +
+                        (timeBlack % 60 > 9 ? timeBlack % 60 : '0' + timeBlack % 60)}</p>
+                    </div>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        backgroundColor: 'whitesmoke',
+                        marginTop: '1VH',
+                        padding: 25
+                    }}>
                         <p id={'White'}>{(timeWhite / 60 > 9 ? Math.trunc(timeWhite / 60) : '0' + Math.trunc(timeWhite / 60)) + ":" +
                         (timeWhite % 60 > 9 ? timeWhite % 60 : '0' + timeWhite % 60)}</p>
                     </div>
-                    <div style={{display: "flex", flexDirection: "row", backgroundColor: 'whitesmoke', marginTop:'1VH', padding:25}}>
+                    {currentTurn === "sw"
+                        ?
+                        <p>{startingTimeWhite}</p>
+                        :
+                        <> </>}
+                </>
+                :
+                <>
+                    {currentTurn === "sw"
+                        ?
+                        <p>{startingTimeWhite}</p>
+                        :
+                        <> </>}
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        backgroundColor: 'whitesmoke',
+                        marginTop: '1VH',
+                        padding: 25
+                    }}>
+                        <p id={'White'}>{(timeWhite / 60 > 9 ? Math.trunc(timeWhite / 60) : '0' + Math.trunc(timeWhite / 60)) + ":" +
+                        (timeWhite % 60 > 9 ? timeWhite % 60 : '0' + timeWhite % 60)}</p>
+                    </div>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        backgroundColor: 'whitesmoke',
+                        marginTop: '1VH',
+                        padding: 25
+                    }}>
                         <p id={'Black'}>{(timeBlack / 60 > 9 ? Math.trunc(timeBlack / 60) : '0' + Math.trunc(timeBlack / 60)) + ":" +
                         (timeBlack % 60 > 9 ? timeBlack % 60 : '0' + timeBlack % 60)}</p>
                     </div>
@@ -139,8 +165,8 @@ const ReactChessClock = (props) => {
                         <p>{startingTimeBlack}</p>
                         :
                         <> </>}
-                    </>
-        }
+                </>
+            }
         </>
     )
 
